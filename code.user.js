f1d92ca2b4b3795b6885608aaa5c91197f834413
Alexandr Stankovich <65312732+AlexStank@users.noreply.github.com>
2020-09-12 09:58:49 +0300

    Update code.user.js

--- code.user.js
+++ code.user.js
@@ -2,22 +2,23 @@
 // @name         On+ Paid Days
 // @namespace    on+_paid_days
 // @version      1.0
-// @description  Подсчёт количества дней с оплаченным интернетом
+// @description  Подсчёт количества оплаченных дней на сайте провайдера ОнПлюс
 // @author       Alexandr Stankovich
 // @match        https://stat.onplus.ru/*
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    let balance = document.querySelector('tr:nth-child(4) > td.boxline.green').innerHTML; // Текущий баланс
-    let request = new XMLHttpRequest(); // Объект запроса
+    let balance = document.querySelector('tr:nth-child(4) > td.boxline.green').innerHTML;
+    let request = new XMLHttpRequest();
 
-    request.open('GET', 'https://stat.onplus.ru/?tact=info/services', true); // Запрос на страницу услуг on+
+    request.open('GET', 'https://stat.onplus.ru/?tact=info/services', true);
     request.onreadystatechange = function () {
-        if (request.readyState == 4 && request.status == 200) { // Если запрос завершён и ответ получен...
-            let sentDOM = new DOMParser().parseFromString(request.responseText, 'text/html'), // Получение элементов со страницы услуг on+
-                cost = sentDOM.querySelector('tr.boxline > td:nth-child(1)').innerHTML, // Нахождение цены за тариф
-                paidDays = Math.ceil(balance.replace(',', '.')/cost.replace(',', '.')*30); // Рассчёт количества оплаченных дней
+        if (request.readyState == 4 && request.status == 200) {
+            let parser = new DOMParser(),
+                setDOM = parser.parseFromString(request.responseText, 'text/html'),
+                cost = sentDOM.querySelector('tr.boxline > td:nth-child(1)').innerHTML,
+                paidDays = Math.ceil(balance.replace(',', '.')/cost.replace(',', '.')*30);
 
             document.querySelector('tr:nth-child(4) > td.boxline.green').innerHTML = balance + ' (дней с интернетом: ' + paidDays + ', включая этот)';
         }
